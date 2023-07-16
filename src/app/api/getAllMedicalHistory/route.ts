c453d8dd21675c72db4041d5e9fdc971bea96306
Zachary <leeyihong03@gmail.com>
2023-07-16 18:12:31 -0400

    get existing history api

---
+++ src/app/api/getAllMedicalHistory/route.ts
@@ -0,0 +1,28 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+    try {
+        const { userId } = auth();
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+        const entireHistory = await prismadb.medicalHistory_DA.findMany({
+            where: {
+                patientId: userId,
+            },
+        });
+        const historyItems = entireHistory.map(
+            ({ id, medicalHistoryType, value }) => ({
+                id,
+                medicalHistoryType,
+                value,
+            })
+        );
+        return NextResponse.json(historyItems);
+    } catch (error) {
+        console.log("GET_PROFILE: ", error);
+        return new NextResponse("Internal error", { status: 500 });
+    }
+};
