c531da92cbede5e5a41374cec63f29c2fe7ef50a
Zachary <leeyihong03@gmail.com>
2023-07-19 12:54:22 -0400

    added theme and logout

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -2,6 +2,8 @@
 import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export function MainNav({
     className,
@@ -37,26 +39,33 @@ export function MainNav({
         },
     ];
     return (
-        <nav
-            className={cn(
-                "flex items-center space-x-4 lg:space-x-6",
-                className
-            )}
-        >
-            {routes.map((route) => (
-                <Link
-                    key={route.href}
-                    href={route.href}
-                    className={cn(
-                        "text-sm font-mdium transition-colors hover:text-primary",
-                        route.active
-                            ? "text-black dark:text-white font-bold"
-                            : "text-muted-foreground"
-                    )}
-                >
-                    {route.label}
-                </Link>
-            ))}
-        </nav>
+        <div className="flex justify-between items-center w-full">
+            <nav
+                className={cn(
+                    "flex items-center space-x-4 lg:space-x-6",
+                    className
+                )}
+            >
+                {routes.map((route) => (
+                    <Link
+                        key={route.href}
+                        href={route.href}
+                        className={cn(
+                            "text-sm font-mdium transition-colors hover:text-primary",
+                            route.active
+                                ? "text-black dark:text-white font-bold"
+                                : "text-muted-foreground"
+                        )}
+                    >
+                        {route.label}
+                    </Link>
+                ))}
+            </nav>
+
+            <div className="flex items-center space-x-4">
+                <ThemeToggle />
+                <UserButton afterSignOutUrl="/" />
+            </div>
+        </div>
     );
 }
